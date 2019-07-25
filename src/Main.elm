651e5edb6c4b37182855693d4f21de4cc678838a
Roman Guivan <roman_omnius@omnius.com>
2019-07-25 15:09:50 +0200

    OK we can move the board

--- src/Main.elm
+++ src/Main.elm
@@ -1,41 +1,132 @@
-import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+port module Main exposing (gameLoopPort, renderUpdatePort)
 
+import Browser
+import Html exposing (Html, Attribute, span, div, p, text)
+import Html.Attributes exposing (class)
+import Keyboard exposing (RawKey)
+import Json.Encode as E
+
+-- from pixi to Elm
+port gameLoopPort: (String -> msg) -> Sub msg
+-- from Elm to pixi
+port renderUpdatePort: String -> Cmd msg
+
+boardSpeedAcclPx: Float
+boardSpeedAcclPx = 12.0
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
 
 
 -- MODEL
+type KeyState = Up | Down
 
-type alias Model = Int
+type alias BoardState = {
+  acceleration: Float,
+  x: Float}
 
-init : Model
-init =
-  0
+type alias ControlsState = {
+      left: KeyState,
+      right: KeyState}
+
+type alias GameState = {
+    board: BoardState,
+    controls: ControlsState}
+
+type alias Model = {
+  pressedKeys: List String,
+  gameState: GameState}
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  (Model [] {
+    controls = { left = Up, right = Up },
+    board = { acceleration = 0, x = 0  }
+  }, Cmd.none)
 
 
 -- UPDATE
 
-type Msg = Increment | Decrement
+type Msg = KeyDown RawKey | KeyUp RawKey | GameFrame | ErrorMsg
 
-update : Msg -> Model -> Model
+keysToControls: Model-> RawKey -> KeyState -> ControlsState
+keysToControls model rawKey direction =
+  case (Keyboard.anyKeyUpper rawKey) of 
+    Just key -> case key of 
+      (Keyboard.Character a) -> case a of 
+        ("A") -> ControlsState direction model.gameState.controls.right
+        ("D") -> ControlsState model.gameState.controls.left direction
+        _ -> model.gameState.controls
+      _ -> model.gameState.controls
+    Nothing -> model.gameState.controls
+
+calcBoardAcceleration: ControlsState -> Float -> Float
+calcBoardAcceleration keys currentAcceleration = 
+  if keys.left == Down then boardSpeedAcclPx * -1
+  else if keys.right == Down then boardSpeedAcclPx
+  else currentAcceleration
+
+calcBoardAccelerationAfterTick: Float -> Float
+calcBoardAccelerationAfterTick currentAccl = 
+  if (currentAccl == 0 || Basics.abs currentAccl < 1.001) then 0
+  else let sign = currentAccl / Basics.abs currentAccl in (Basics.sqrt (Basics.abs currentAccl)) * sign
+
+gameLoop: Model -> Model
+gameLoop model = 
+  let boardAcceleration = calcBoardAcceleration model.gameState.controls model.gameState.board.acceleration
+      boardAccelerationAfterTick = calcBoardAccelerationAfterTick boardAcceleration
+      board = model.gameState.board
+      in (Model [(Debug.toString model.gameState.controls)] {
+        controls = model.gameState.controls,
+        board = { x = board.x + boardAcceleration, acceleration = boardAccelerationAfterTick }
+      })
+
+decodeJsMessage: String -> Msg
+decodeJsMessage string =
+  case string of 
+    "GameFrame" -> GameFrame
+    _ -> ErrorMsg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
-    Increment ->
-      model + 1
+      KeyDown rawKey -> 
+        let game = model.gameState 
+            updatedGameState = { game | controls = keysToControls model rawKey Down }
+              in ( { model | pressedKeys =  [(Debug.toString game.controls)]
+              , gameState = updatedGameState }, Cmd.none )
+      KeyUp rawKey -> 
+       let game = model.gameState 
+           updatedGameState = { game | controls = keysToControls model rawKey Up }
+              in ( { model | pressedKeys =  [(Debug.toString game.controls)]
+              , gameState = updatedGameState }, Cmd.none )
+      GameFrame -> (gameLoop model, renderUpdatePort (Debug.toString model.gameState.board.x))
+      ErrorMsg -> (model, Cmd.none) -- i should have visualized it somehow
 
-    Decrement ->
-      model - 1
+-- SUBSCRIPTIONS
 
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.batch [ 
+      Keyboard.downs KeyDown
+    , Keyboard.ups KeyUp
+    , gameLoopPort decodeJsMessage
+  ]
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+  p [class "elm-debug"] [
+      text("Elm Debug:")
+    , p [ ] [text( "Keys pressed: " ++ Debug.toString model.pressedKeys)]
+  ]
+
+-- PORTS
--- src/index.js
+++ src/index.js
@@ -1,43 +1,54 @@
+import * as Rx from 'rxjs'
 import * as PIXI from 'pixi.js'
 import { Elm } from './elm-main'
 
-function component() {
-    const app = new PIXI.Application();
-    // load the texture we need
+(function () {
 
-    app.loader.add('board', 'images/board.png').load((loader, resources) => initializeSprite(app, resources));
-    return app.view;
-}
+    let board, elmApp;
 
-function elmComponent() {
-    const element = document.getElementById('elm');
-
-    if (!element) {
-        throw new Error('Elm element was not found in DOM');
+    function gameLoop () {
+        elmApp.ports.gameLoopPort.send("GameFrame");
     }
 
-    var app = Elm.Main.init({
-        node: element
-    });
-}
-
-function initializeSprite(app, resources) {
-
-    const board = new PIXI.Sprite(resources.board.texture);
-
-    board.x = app.renderer.width / 2;
-    board.y = app.renderer.height / 2;
-
-    board.anchor.x = 0.5;
-    board.anchor.y = 0.5;
-
-    app.stage.addChild(board);
-
-    app.ticker.add(() => {
-        // each frame we spin the bunny around a bit
-        board.rotation += 0.01;
-    });
-}
-
-document.body.appendChild(component());
-elmComponent()
+    function component() {
+        const app = new PIXI.Application();
+        // load the texture we need
+        app.loader.add('board', 'images/board.png').load((loader, resources) => initializeSprite(app, resources));
+        return app.view;
+    }
+    
+    function elmComponent() {
+        const element = document.getElementById('elm');
+    
+        if (!element) {
+            throw new Error('Elm element was not found in DOM');
+        }
+    
+        elmApp = Elm.Main.init({
+            node: element
+        });
+    }
+    
+    function initializeSprite(app, resources) {
+        const elmZeroX = app.renderer.width / 2;
+    
+        board = new PIXI.Sprite(resources.board.texture);
+        board.x = elmZeroX;
+        board.y = app.renderer.height * 0.85;
+    
+        board.anchor.x = 0.5;
+        board.anchor.y = 0.5;
+    
+        app.stage.addChild(board);
+    
+        app.ticker.add(() => gameLoop());
+        elmApp.ports.renderUpdatePort.subscribe(function (data) {
+            // OX: 0 in Elm is app.renderer.width / 2 in renderer, so X == 0 is center of the screen
+            const dX = elmZeroX + Number(data);
+            board.x = dX;
+        });
+    }
+    
+    elmComponent()
+    document.body.appendChild(component());
+} ())
