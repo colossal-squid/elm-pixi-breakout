6c0bbf01b853ecec8986b0fdbfafb9e63b89588b
Roman Guivan <roman_omnius@omnius.com>
2019-07-25 15:32:37 +0200

    index.js is a bit tidier, i hope it makes more sense

--- src/index.js
+++ src/index.js
@@ -2,53 +2,54 @@ import * as Rx from 'rxjs'
 import * as PIXI from 'pixi.js'
 import { Elm } from './elm-main'
 
-(function () {
-
-    let board, elmApp;
-
-    function gameLoop () {
+(async function () {
+    // init pixi side of things first
+    const {pixiApp, board, elmZeroX} = await initPixiApp();
+    // init the ELM part of the game
+    let elmApp = createElmApp('#elm');
+    pixiApp.ticker.add(() => {
+        // PIXI calls this for every game frame
         elmApp.ports.gameLoopPort.send("GameFrame");
+    });
+
+    function initPixiApp() {
+        return new Promise((resolve, reject) => {
+            const app = new PIXI.Application(),
+            elmZeroX = app.renderer.width / 2;
+
+            // load the textures
+            app.loader.add('board', 'images/board.png').load((loader, resources) => {
+                const boardSprite = new PIXI.Sprite(resources.board.texture);
+
+                boardSprite.x = elmZeroX;
+                boardSprite.y = app.renderer.height * 0.85;
+
+                boardSprite.anchor.x = 0.5;
+                boardSprite.anchor.y = 0.5;
+                app.stage.addChild(boardSprite);
+                resolve({pixiApp: app, board: boardSprite, elmZeroX: elmZeroX });
+            });
+        })
     }
 
-    function component() {
-        const app = new PIXI.Application();
-        // load the texture we need
-        app.loader.add('board', 'images/board.png').load((loader, resources) => initializeSprite(app, resources));
-        return app.view;
-    }
-    
-    function elmComponent() {
-        const element = document.getElementById('elm');
-    
-        if (!element) {
-            throw new Error('Elm element was not found in DOM');
+    function createElmApp(selector) {
+        const elmElement = document.querySelector(selector);
+
+        if (!elmElement) {
+            throw new Error(`Elm element (${selector}) was not found in DOM`);
         }
-    
-        elmApp = Elm.Main.init({
-            node: element
-        });
+
+        const elmApp =  Elm.Main.init({ node: elmElement});
+        elmApp.ports.renderUpdatePort.subscribe((data) => render(data) );
+        return elmApp;
     }
-    
-    function initializeSprite(app, resources) {
-        const elmZeroX = app.renderer.width / 2;
-    
-        board = new PIXI.Sprite(resources.board.texture);
-        board.x = elmZeroX;
-        board.y = app.renderer.height * 0.85;
-    
-        board.anchor.x = 0.5;
-        board.anchor.y = 0.5;
-    
-        app.stage.addChild(board);
-    
-        app.ticker.add(() => gameLoop());
-        elmApp.ports.renderUpdatePort.subscribe(function (data) {
-            // OX: 0 in Elm is app.renderer.width / 2 in renderer, so X == 0 is center of the screen
-            const dX = elmZeroX + Number(data);
-            board.x = dX;
-        });
+
+    // This function is a callback from Elm's game loop into pixi
+    function render(data) {
+        // OX: 0 in Elm is app.renderer.width / 2 in renderer, so X == 0 is center of the screen
+        const dX = elmZeroX + Number(data);
+        board.x = dX;
     }
-    
-    elmComponent()
-    document.body.appendChild(component());
+
+    document.body.appendChild(pixiApp.view);
 } ())
